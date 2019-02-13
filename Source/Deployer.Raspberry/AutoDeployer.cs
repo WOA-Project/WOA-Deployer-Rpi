76187997036b1dfcaf415152c5c9f8936ac00c6a
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-02-13 09:14:12 +0100

    Script and license

--- Source/Deployer.Raspberry/AutoDeployer.cs
+++ Source/Deployer.Raspberry/AutoDeployer.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Threading.Tasks;
 using Deployer.Execution;
 
@@ -8,30 +7,21 @@ namespace Deployer.Raspberry
     public class WoaDeployer : IWoaDeployer
     {
         private readonly ITooling tooling;
-        private readonly IPhone phone;
         private readonly IScriptRunner scriptRunner;
         private readonly IScriptParser parser;
 
-        public WoaDeployer(IScriptRunner scriptRunner, IScriptParser parser, ITooling tooling, IPhone phone)
+        public WoaDeployer(IScriptRunner scriptRunner, IScriptParser parser, ITooling tooling)
         {
             this.scriptRunner = scriptRunner;
             this.parser = parser;
             this.tooling = tooling;
-            this.phone = phone;
         }
 
         public async Task Deploy()
         {
-            var dict = new Dictionary<PhoneModel, string>
-            {
-                {PhoneModel.Talkman, Path.Combine("Scripts", "950.txt")},
-                {PhoneModel.Cityman, Path.Combine("Scripts", "950xl.txt")},
-            };
-
-            var phoneModel = await phone.GetModel();
-            var path = dict[phoneModel];
-
-            await scriptRunner.Run(parser.Parse(File.ReadAllText(path)));
+            var scriptPath = Path.Combine("Scripts", "Deployment.txt");
+            
+            await scriptRunner.Run(parser.Parse(File.ReadAllText(scriptPath)));
         }
 
         public async Task InstallGpu()
