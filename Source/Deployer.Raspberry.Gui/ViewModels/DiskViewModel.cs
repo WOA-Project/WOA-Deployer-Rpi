c8f56e0d54769e4e111119cd9e65837784a7e81e
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-02-13 08:32:27 +0100

    Previous to modifying the ViewModels

---
+++ Source/Deployer.Raspberry.Gui/ViewModels/DiskViewModel.cs
@@ -0,0 +1,21 @@
+using ByteSizeLib;
+using Deployer.FileSystem;
+
+namespace Deployer.Raspberry.Gui.ViewModels
+{
+    public class DiskViewModel
+    {
+        private readonly Disk disk;
+
+        public DiskViewModel(Disk disk)
+        {
+            this.disk = disk;
+        }
+
+        public uint Number => disk.Number + 1;
+        public string FriendlyName => disk.FriendlyName;
+        public ByteSize Size => disk.Size;
+        public bool IsUsualTarget => Size > ByteSize.FromGigaBytes(14) && Size < ByteSize.FromGigaBytes(255);
+        public Disk Disk => disk;
+    }
+}
