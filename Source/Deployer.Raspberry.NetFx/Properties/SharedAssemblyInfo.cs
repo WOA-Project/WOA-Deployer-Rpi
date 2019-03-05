040a776389332840f333efd86ed8a2a2bb79e24e
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-03-05 12:11:35 +0100

    Updated version 2.1.1

--- Source/Deployer.Raspberry.NetFx/Properties/SharedAssemblyInfo.cs
+++ Source/Deployer.Raspberry.NetFx/Properties/SharedAssemblyInfo.cs
@@ -27,5 +27,5 @@
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyVersion("2.1.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
